9dcb3ac110efc6710775da842992e5d10fe97840
rickwayne1125 <1037457338@qq.com>
2022-12-15 15:36:04 -0500

    ADD build shell script

---
+++ build_run.sh
@@ -0,0 +1,13 @@
+echo "Clearing old build files..."
+rm -r bin
+echo "Done"
+echo "Compiling..."
+find ./src/* | grep .java > sources.txt
+javac -d bin -cp lib/sqlite-jdbc-3.36.0.3.jar:lib/forms_rt-7.0.3.jar @sources.txt
+echo "Done"
+echo "Generating Jar Package"
+# Generate Manifest
+cat 'MANIFEST.MF'
+# Generate jar file
+jar -cvfm 'FancyBank611.jar' 'MANIFEST.MF' -C 'bin' .
+echo "Done"
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,21 @@
+import DataBase.DataBase;
 import Frontend.ViewFactory;
 import Frontend.Frontend;
+import Utils.Config;
+
+import java.io.File;
 
 public class Main {
     public static void main(String[] args) {
+        // Check if the database exists
+        String dbPath = Config.DB_URL.split(":")[2];
+        if (!new File(dbPath).exists()) {
+            System.out.println("Database not found. Initializing database...");
+            // Initialize database
+            DataBase.clearDatabase();
+            DataBase.createTables();
+            DataBase.generateTestData();
+        }
         Frontend.getInstance(ViewFactory.getLoginPage());
     }
 }
